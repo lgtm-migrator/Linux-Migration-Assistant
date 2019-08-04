0d86e0a035f3e7ea1bb50742051eb4e7102074f0
youssef <youss.hark@gmail.com>
2019-08-04 00:22:34 -0400

    Created branch for listing snap packages

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 setup(
     name="Linux Migration Assistant",
     version="0.0.1",
+    license="GPL-3.0",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(exclude=['*contrib*', '*docs*', '*tests*']),
+    python_requires='>=3',
 )
