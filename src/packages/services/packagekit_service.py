fb31efc2ac0c3a673f17afdd4f611e3741d8b4cf
Jeff Labonte <jeff.labonte12@gmail.com>
2019-08-13 00:07:10 -0400

    Build unittest, list installed packages from apt

--- src/packages/services/packagekit_service.py
+++ src/packages/services/packagekit_service.py
@@ -1,25 +1,36 @@
-import gi
 from typing import Callable
+
+from packages.formatter.package_formatter import PackageFormatter
+
+import gi
 
 gi.require_version("PackageKitGlib", "1.0")
 from gi.repository import PackageKitGlib
 
-from packages.formatter.package_formatter import PackageFormatter
-
 
 class PackageKitService:
+    """
+        Regroup common command you would like to use
+        with PackageKit
+    """
+
     def __init__(self, progress_publisher=None):
         self.packagekit_client = PackageKitGlib.Client().new()
         self.progress_publisher = progress_publisher
 
-    def progress_callback(self, status, typ, data=None):
-        pass
-
-    def callback_ready(self):
-        pass
-
-    def list_installed_packages(self):
-        pass
+    def list_installed_packages(
+        self, progress_callback: Callable, progress_user_data: object
+    ):
+        return (
+            p
+            for p in self.packagekit_client.get_packages(
+                filters=PackageKitGlib.FilterEnum.from_string("INSTALLED"),
+                cancellable=None,
+                progress_callback=progress_callback,
+                progress_user_data=progress_user_data,
+            ).get_package_array()
+            if "installed" in p.get_data()
+        )
 
     def install_package(self, name: str) -> None:
         # TODO Requires package_name;version;arch;distro as a string to install
