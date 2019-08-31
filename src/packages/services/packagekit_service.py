99ade76140cae975a327e81bfb75785498590aa5
Jeff Labonte <jeff.labonte12@gmail.com>
2019-08-30 20:14:21 -0400

    Update pydoc of list_installed_package

--- src/packages/services/packagekit_service.py
+++ src/packages/services/packagekit_service.py
@@ -22,10 +22,8 @@ class PackageKitService:
         self, progress_callback: Callable, progress_user_data: object
     ) -> Generator:
         """
-
-        :param progress_callback: function
-        :param progress_user_data:  tuple
-        :return: generator
+            List the package that are installed on your
+            device
         """
         return (
             p
@@ -44,50 +42,3 @@ class PackageKitService:
             self.packagekit_client.install_package()
         except Exception as ex:
             print(ex)
-
-    def _create_dict_from_array(self, package_array):
-        """_create_dict_from_array
-
-        extract data from each Package found
-
-        :param package_array: [ PackageKitGlib.Package, ...]
-        :returns : list of dictionnaries with all the informations in the Package
-        """
-        packages = []
-        for package in package_array:
-            packages.append(self._extract_package_to_dict(package=package))
-        return packages
-
-    @staticmethod
-    def _extract_package_to_dict(package):
-        """_extract_package_to_dict
-
-        Use the formatter to return a dictionnary
-        filled all the informations found in the Package object
-
-        :param package: PackageKitGlib.Package
-        :returns dict: Dictionnary of the information in the Package
-        """
-        return PackageFormatter.format_package_informations(
-            id_package=package.get_id(),
-            name=package.get_name(),
-            platform=package.get_arch(),
-            source=package.get_data(),
-            package_type="apt",  # TODO Should in a variable ( find package manager )
-            version=package.get_version(),
-            is_installed=package.get_data(),
-        )
-
-    def _extract_information_from_strings(self, package):
-        """__extract_information_from_strings
-
-            Some Package informations are stored in
-            strings or Enums. This function
-            has for purpose to extract those embedded informations
-
-        :doc: https://lazka.github.io/pgi-docs/#PackageKitGlib-1.0/classes/Package.html#PackageKitGlib.Package
-        :param package:
-        :return TBD -> TODO find out what can be extracted
-        """
-        # TODO Extract data from the Package Data
-        pass
