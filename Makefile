0288bf659ec35bd386d2af68a3065c3baad4cda1
youssef <youss.hark@gmail.com>
2019-09-12 23:37:58 -0400

    WIP

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ build:
 
 setup_dev:
 	make build
-	python3 -m venv	.venv && source .venv/bin/activate && pip install vext vext.gi
+	python3 -m venv	.venv && source .venv/bin/activate && pip install vext vext.gi && pip install requirements/dev.txt
 
 test:
 	cd src && python3 -m unittest discover tests -v
--- src/packages/services/snap_service.py
+++ src/packages/services/snap_service.py
@@ -1,6 +1,36 @@
-from src.utils.cmds_utils import AbstractCommand
+from typing import Dict, List
+
+import gi
+
+gi.require_version("Snapd", "1")
+from gi.repository import Snapd
+
+class SnapService:
+
+    def __init__(self):
+        self.snap = Snapd.Client().new()
 
 
-Class SnapService(AbstractCommand):
+    def list_packages(self) -> List:
+        """
+          list the packages installed on this machine
+        """
+        installed_snaps = self.snap_client.get_snaps_sync(flags=0, names=None)
+        return installed_snaps
 
 
+    def install_package(self, package):
+        """
+        Install each package which is inside the list
+        :param package: name of the snap package
+
+        """
+        self.snap.install2_sync(name=package)
+
+
+    def delete_package(self, package):
+        """
+        Delete package passed as args
+        :param package: package to delete
+        """
+        self.snap_client.remove_sync(name=package)
