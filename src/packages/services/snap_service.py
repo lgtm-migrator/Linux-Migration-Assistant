f158377f6c74b235f369666feeac077252c5b273
youssef <youss.hark@gmail.com>
2019-10-09 00:02:39 -0400

    Added test for installation of snap

--- src/packages/services/snap_service.py
+++ src/packages/services/snap_service.py
@@ -14,6 +14,26 @@ class SnapService:
     def list_installed_packages(self) -> List:
         """
           list the packages installed on this machine
+         :return: List
         """
         return self.snap.get_snaps_sync(flags=0, names=None)
 
+    def list_one_package(name: str) -> Snapd.Snap:
+        """
+          Get information of a single installed snap. If the snap does not exist an error occurs
+        :return: Snapd.Snap
+        """
+
+    def install_package_name(self, flags, name, channel, revision, progress_callback, pgd, clble) -> bool:
+        """
+        Install package with the name
+        :param package: name of the snap package
+        """
+        return self.snap.install2_sync(flags, name, channel, revision, progress_callback, pgd, clble)
+
+    def delete_package(self, name: str):
+        """
+         Delete package passed as args
+        :param package: package to delete
+        """
+        self.snap_client.remove_sync(name)
--- src/tests/test_snap_package_service.py
+++ src/tests/test_snap_package_service.py
@@ -4,6 +4,7 @@ import gi
 gi.require_version("Snapd", "1")
 from gi.repository import Snapd
 
+snap_list_toplaywith = ["spotify", "krop"]
 
 class TestSnapPackageService:
     SNAP_CLIENT = SnapService()
@@ -16,3 +17,19 @@ class TestSnapPackageService:
             print(package.get_name())
             assert package.get_install_date() is not None
             assert isinstance(package, Snapd.Snap)
+
+
+    def test_install_package(self):
+        for idx,snap in enumerate(snap_list_toplaywith):
+            check_snap = SnapService.list_one_package(snap)
+            if check_snap is None:
+                flags = Snapd.InstallFlags.NONE  # CLASSIC, DEVMODE, DANGEROUS, JAILMODE
+                check_snap = snap_list_toplaywith[idx]
+                channel = None  # default channel
+                revision = None  # default revision
+                progress_callback = None
+                new_installed_snap = self.SNAP_CLIENT.install_package_name(flags, check_snap, channel, revision, progress_callback, None, None)
+                assert new_installed_snap is True
+                print("snap installations and tests went ok, now we delete the tested snaps")
+        # for snap in snap_list_toplaywith:
+        #     self.SNAP_CLIENT.delete_package(snap)
