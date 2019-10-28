97d6cf206bbf7f54427f55f67aeb84397cd0c73a
youssef <youss.hark@gmail.com>
2019-10-27 20:16:02 -0400

    Made the changes following pr comments

--- src/tests/test_snap_package_service.py
+++ src/tests/test_snap_package_service.py
@@ -1,13 +1,8 @@
-from src.packages.services.snap_service import SnapService
-import logging
+from packages.services.snap_service import SnapService
 import gi
-import pytest
 
 gi.require_version("Snapd", "1")
 from gi.repository import Snapd
-
-logging.basicConfig(filename='app.log', filemode='w', format='%(process)d - %(asctime)s - %(levelname)s - %(message)s')
-
 
 
 class TestSnapPackageService:
@@ -22,37 +17,22 @@ class TestSnapPackageService:
             assert isinstance(package, Snapd.Snap)
 
     def test_install_package(self):
-        for idx, snap in enumerate(self.snap_list_toplaywith):
-            name = str(snap)
-            try:
-                check_snap = self.SNAP_CLIENT.list_one_package(name)
-            except Exception as e:
-                if e.domain == 'snapd-error-quark':
-                    flags = Snapd.InstallFlags.NONE  # CLASSIC, DEVMODE, DANGEROUS, JAILMODE
-                    check_snap = self.snap_list_toplaywith[idx]
-                    channel = None  # default channel
-                    revision = None  # default revision
-                    progress_callback = None
-                    new_installed_snap = self.SNAP_CLIENT.install_package_name(flags, check_snap, channel, revision,
-                                                                               progress_callback, None, None)
-                    assert new_installed_snap is True
-                else:
-                    pytest.fail("Exception was raised", e)
-
+        for snap in self.snap_list_toplaywith:
+            flags = Snapd.InstallFlags.NONE  # CLASSIC, DEVMODE, DANGEROUS, JAILMODE
+            channel = None  # default channel
+            revision = None  # default revision
+            progress_callback = None
+            new_installed_snap = self.SNAP_CLIENT.install_package_name(flags, snap, channel, revision,
+                                                                       progress_callback, None, None)
+            assert new_installed_snap is True
 
     def test_remove_package(self):
-        for idx, snap in enumerate(self.snap_list_toplaywith):
-            print("checking if snap package named " + snap + " exists")
-            check_snap = self.SNAP_CLIENT.list_one_package(snap)
-            if check_snap is not None:
-                check_snap = str(self.snap_list_toplaywith[idx])
-                print("check snap " + check_snap)
-                progress_callback = None
-                progress_callback_data = None
-                cancellable = None
-                removed_snap = self.SNAP_CLIENT.delete_package(check_snap, progress_callback,
-                                                               progress_callback_data,
-                                                               cancellable)
-                assert removed_snap is True
-            else:
-                pytest.fail("Test failed because the required packages are not present in this machine")
+        for snap in self.snap_list_toplaywith:
+            progress_callback = None
+            progress_callback_data = None
+            cancellable = None
+            removed_snap = self.SNAP_CLIENT.delete_package(snap, progress_callback,
+                                                           progress_callback_data,
+                                                           cancellable)
+            assert removed_snap is True
+
