dbb0869977651f814cc03e0813903f6c164a1e58
Jeff Labonte <jeff.labonte12@gmail.com>
2020-07-14 01:33:31 -0400

    Add description and application name to CLI

--- src/app.py
+++ src/app.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from argparse import ArgumentParser
+
+
+SOFTWARE_NAME = "Linux Migration Assistant - CLI"
+SOFTWARE_DESCRIPTION = """
+This tool is there to help you make the migration of your Linux Machine to your
+new Linux machine. We want to help provide the same functionality that macOS has.
+"""
+
+
+if __name__ == "__main__":
+    args = ArgumentParser(prog=SOFTWARE_NAME, description=SOFTWARE_DESCRIPTION)
