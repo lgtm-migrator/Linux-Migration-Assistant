d49ca75e2719a9b550a25faf89e866658abfe5ad
Jeff Labonte <grimsleepless@protonmail.com>
2020-08-13 17:50:28 -0400

    Create helper to handle blocks and chunks

--- src/helpers/backup_helper.py
+++ src/helpers/backup_helper.py
@@ -1,14 +1,36 @@
-from hashlib import md5
-from zlib import adler32
+from backups.chunks import Chunk, Signature
+from .hash_helper import compute_md5, compute_adler32
+
+from pathlib import Path
 
 BLOCK_SIZE = 4096
 
 
-def compute_md5(chunk):
-    return md5(chunk).hexdigest()
+def checksums_file(file_path: Path):
+    chunks = Chunk()
+    with open(file_path) as f:
+        while True:
+            chunk = f.read(BLOCK_SIZE)
+            if not chunk:
+                break
+            chunks.append(
+                Signature(
+                    adler32=compute_adler32(chunk),
+                    md5=compute_md5(chunk),
+                )
+            )
+    return chunks
 
 
-def compute_adler32(chunk):
-    return adler32(chunk)
+def has_different_blocks(src, dst):
+    is_different = True
+    checksum_file_two = checksums_file(dst)
+    with open(src) as f:
+        while True:
+            chunk = f.read(BLOCK_SIZE)
+            if not chunk:
+                break
 
-
+            if checksum_file_two.get_chunk(chunk=chunk) is None:
+                return is_different
+    return not is_different
