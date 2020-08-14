03fdff7564cbfd503035bde3848abb2e5dca5ca2
Jeff Labonte <grimsleepless@protonmail.com>
2020-08-13 21:21:47 -0400

    Fix Test, simplify test result

--- src/tests/integration/tests_helper/test_backup_helper.py
+++ src/tests/integration/tests_helper/test_backup_helper.py
@@ -9,8 +9,8 @@ CURRENT_FOLDER = Path(__file__).resolve().parent
 
 @pytest.mark.parametrize('file_to_compare,is_diff',[
     (f'{CURRENT_FOLDER}/test_file/same.txt', []),
-    (f'{CURRENT_FOLDER}/test_file/diff.txt', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]),
+    (f'{CURRENT_FOLDER}/test_file/diff.txt', [16]),
 ])
 def test_has_different_blocks(file_to_compare, is_diff):
-    original_file_path = f'{CURRENT_FOLDER}/tests_helper/test_file/original.txt'
+    original_file_path = f'{CURRENT_FOLDER}/test_file/original.txt'
     assert get_different_blocks(src=original_file_path, dst=file_to_compare) == is_diff
